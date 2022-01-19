4b66c8c5467c45927cfd4510b59458da47e23791
Weng Fei Fung <weffung@ucdavis.edu>
2022-01-18 21:35:45 -0800

    Instructions: Text and image swapping in place (Todo: Spacing between text and image too static

--- Mixo/VC/InstructionsVC.swift
+++ Mixo/VC/InstructionsVC.swift
@@ -12,6 +12,7 @@ import SwiftGifOrigin
 @available(iOS 13.0, *)
 class InstructionsVC: UIViewController{
     
+    @IBOutlet weak var tvInstructions: UITextView!
     @IBOutlet weak var ivInstructions: UIImageView!
 //    @IBOutlet weak var btnLoadEngine: UIButton!
     
@@ -19,8 +20,27 @@ class InstructionsVC: UIViewController{
     @IBOutlet var uiView: UIView!
 //    @IBOutlet weak var swipeText: UITextView!
 //    @IBOutlet weak var arrowText: UITextView!
+        
+    let instructionList = [
+        "The MixoType Engine is a self-assessment that helps you visualize your unique identity through a symbol we call, your MixoType.",
+
+        "A Mixotype Identity represents your Heroes, Intelligences, Talents, and Collections, which are what you value that speak to who you are.",
+
+        "In each section, you respond to the prompt by selecting the icons from the options presented in order of their significance to you, from 1 (highest) to 4 (lowest).",
+
+        "In the Heroes, Talents, and Collections sections, you will select an additional Temperament or Modality icon that aligns with each of your primary selections.",
+
+        "After each section, you get a recap of your selections, plus the option to start over if you want to make any changes."
     
-    let imageList = ["instructions1", "instructions2", "instructions3", "instructions4", "instructions5"]
+    ]
+    let imageList = [
+        "instructions-alt-0",
+        "instructions-alt-1",
+        "instructions-alt-2",
+        "instructions-alt-3",
+        "instructions-alt-4",
+    ]
+
     var index = 0
     
     override func viewDidLoad() {
@@ -29,70 +49,33 @@ class InstructionsVC: UIViewController{
         // Do any additional setup after loading the view.
         
         setUpUI()
-        
-//        let SwipeLeft = UISwipeGestureRecognizer()
-//        let SwipeRight = UISwipeGestureRecognizer()
-        
-//        SwipeLeft.direction = .left
-//        SwipeRight.direction = .right
-//
-//        self.uiView.addGestureRecognizer(SwipeLeft)
-//        self.uiView.addGestureRecognizer(SwipeRight)
-//
-//        SwipeLeft.addTarget(self, action: #selector(Swipe(sender:)))
-//        SwipeRight.addTarget(self, action: #selector(Swipe(sender:)))
-        
-        
     }
-    
-//    @objc func Swipe(sender: UISwipeGestureRecognizer){
-//        switch sender.direction{
-//            case .left:
-//                if index < imageList.count - 1 {
-//                    index = index + 1
-//                    ivInstructions.loadGif(name: imageList[index])
-//                }
-//                if index < 4 {
-//                    btnLoadEngine.alpha = 0
-//                } else {
-//                    btnLoadEngine.alpha = 1
-//                    swipeText.alpha = 0
-//                    arrowText.alpha = 0
-//                }
-//            case .right:
-//                if index > 0 {
-//                    index = index - 1
-//                    ivInstructions.loadGif(name: imageList[index])
-//                }
-//
-//                if index < 4 {
-//                    btnLoadEngine.alpha = 0
-//                } else {
-//                    btnLoadEngine.alpha = 1
-//                }
-//            default:
-//                break
-//        }
-//    }
+
     
     func setUpUI() {
         
-        btnContinueNext.setTitle("Continue", for: .normal)
-        ivInstructions.loadGif(name: imageList[0])
+        btnContinueNext.setTitle("Next", for: .normal)
+        
+        // index 0
+        tvInstructions.text = instructionList[index]
+        ivInstructions.loadGif(name: imageList[index])
         
     }
     
     @IBAction func btnContinueNext(_ sender: Any) {
-
+        
+        // On the last instructions page, change "Next" button to a "Continue" button that would load MX engine.
         index = index + 1
-        if(index < 5) {
-            btnContinueNext.setTitle("Continue", for: .normal)
-            ivInstructions.loadGif(name: imageList[index])
-        } else if (index == 5) {
+        if(index == 5) {
             let mixoEngineLoad = mainSB.instantiateViewController(withIdentifier: "EngineLoadVC") as! EngineLoadVC
             self.present(mixoEngineLoad, animated: true, completion: nil)
+        } else if(index == 4) {
+            btnContinueNext.setTitle("Continue", for: .normal)
+            tvInstructions.text = instructionList[index]
+            ivInstructions.image = UIImage(named:imageList[index])
         } else {
-            ivInstructions.loadGif(name: imageList[index])
+            tvInstructions.text = instructionList[index]
+            ivInstructions.image = UIImage(named:imageList[index])
         }
         
     }
