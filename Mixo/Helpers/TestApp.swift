c7e184100d0417ebfecdf85aa29362cc375b6590
Weng Fei Fung <weffung@ucdavis.edu>
2022-07-02 01:17:13 -0700

    Ready to refactor passed tests into Discovery to test sparks and mixes

--- Mixo/Helpers/TestApp.swift
+++ Mixo/Helpers/TestApp.swift
@@ -10,9 +10,9 @@ import Foundation
 /* Important. Set environment!
  * If development mode, then we can code screen skips for faster testing
  */
-let devMode = true; // skips before-breathing screen and lets user skip breathing screen, which takes time
-let skipToScreen = true; // if true, go to ViewController.swift:skipToScreenAt() to setup which screen
-let autoLogin = false; // if true, go to reference to setup your credentials
+let devMode = true; // faster UX flow by automatically skipping irrelevant screens inbetween, like before-breathing screen and giving you the button to skip the breathing screen (rather than forcing you to wait)
+let skipToScreen = false; // if true, go to ViewController.swift:skipToScreenAt() to setup which screen. Load test driven screens here if applicable.
+let autoLogin = true; // if true, automatically logs in and goes to relevant screen after logging in. Setup your credentials at ViewController.swift searching for "autologin"
 
 
 class TestApp {
